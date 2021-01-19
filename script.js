07df494127fce3c38a1fdb3b36a9e46a0067f264
n8body <1971n8b8dy@users.noreply.github.com>
2021-01-19 11:05:47 +0200

    1.0.0
    Absolutely new todo list.  Using Vue JS

--- script.js
+++ script.js
@@ -1,103 +1,50 @@
-const input = document.getElementById('input'),
-      list = document.getElementById('todo-list');
-
-const todos = JSON.parse(localStorage.getItem('todos')) || {};
-
-const dtButton = document.getElementById('dark-theme-button');
-
-const save = function(key) {
-  localStorage.setItem('todos', JSON.stringify(todos));
-},
-addTodo = function(value) {
-  if (!todos.hasOwnProperty(value)) {
-    todos[value] = {value: input.value, checked: false,};
-  }
-
-  let item = document.createElement('div');
-  item.innerHTML = `<div class="checkbox"></div><span class="text"></span><div class="delete">X</div>`;
-  item.getElementsByClassName('text')[0].innerText = value;
-  item.className = 'todo-item';
-  list.insertAdjacentElement('afterbegin', item);
-
-  save();
-},
-deleteTodo = function(item) {
-  delete todos[(item.getElementsByClassName('text')[0].innerText)];
-  item.remove();
-  save();
-},
-markAsDone = function(item) {
-  if (item.classList.contains('done')) {
-    item.previousElementSibling.innerText = '';
-    item.closest('.todo-item').style.opacity = 1;
-    item.classList.remove('done');
-    todos[item.innerText].checked = false;
-
-    save();
-    return;
-  }
-  todos[item.innerText].checked = true;
-  item.previousElementSibling.innerText = '✔';
-  item.closest('.todo-item').style.opacity = 0.7;
-  item.classList.add('done');
-  
-  save();
-},
-switchThemes = function() {
-  let style = document.querySelector('link[href="dark-style.css"]');
-  if (style) {
-    dtButton.innerHTML = 'Off';
-    style.remove();
-    localStorage.setItem('dark-theme', 'false');
-  } else {
-    let addedStyle = document.createElement('link');
-    addedStyle.rel = 'stylesheet';
-    addedStyle.href = 'dark-style.css';
-    document.head.insertAdjacentElement('beforeend', addedStyle);
-    dtButton.innerHTML = 'On';
-    localStorage.setItem('dark-theme', 'true');
-  }
+const App = {
+	data() {
+		return {
+			title: 'Todo list',
+			inputPlaceholder: 'Type in your task...',
+			inputValue: '',
+			todos: JSON.parse(localStorage.getItem('todos-list-vue')) || [],
+			doneTasks: JSON.parse(localStorage.getItem('doneTasks-list-vue')) || [],
+		};
+	},
+	methods: {
+		inputChangeHandler(e) {
+			this.inputValue = e.target.value;
+		},
+		addTodo(e) {
+			if (this.inputValue == '') {
+				return;
+			}
+			this.todos.push(this.inputValue);
+			this.inputValue = '';
+		},
+		inputKeyUp(e) {
+			this.addTodo();
+		},
+		markAsDone(i) {
+			this.doneTasks.push(this.todos.splice(i, 1)[0]);
+		},
+		markAsUnDone(i) {
+			this.todos.push(this.doneTasks.splice(i, 1)[0]);
+		},
+		deleteTask(i, place) {
+			if (!confirm('Are you sure?')) {
+				return;
+			}
+			this[place].splice(i, 1);
+		},
+		clearList(place) {
+			if (!confirm('Are you sure?')) {
+				return;
+			}
+			this[place] = [];
+		},
+	},
 };
+const vm = Vue.createApp(App).mount('#todo-app');
 
-if (localStorage.getItem('dark-theme') === 'true') {
-  switchThemes();
-}
-
-for (let todo in todos) {
-  addTodo(todos[todo].value);
-}
-for (let i of document.getElementsByClassName('text')) {
-  if (todos[i.innerText].checked) {
-    markAsDone(i);
-  }
-}
-
-const enterEvent = function(key) {
-  if (key.code != 'Enter' || input.value.trim() == '') {return;}
-  addTodo(input.value);
-  input.value = '';
-}
-
-input.addEventListener('focus', function() {
-  input.addEventListener('keyup', enterEvent);
+window.addEventListener('beforeunload', () => {
+	localStorage.setItem('todos-list-vue', JSON.stringify(vm.todos));
+	localStorage.setItem('doneTasks-list-vue', JSON.stringify(vm.doneTasks));
 });
-input.addEventListener('blur', function() {
-  input.removeEventListener('keyup', enterEvent);
-});
-document.getElementById('add-button').addEventListener('click', function() {
-  if (input.value.trim() == '') {return;}
-  addTodo(input.value);
-  input.value = ''; 
-});
-
-list.addEventListener('click', function(e) {
-  if (e.target.classList.contains('delete')) {
-    deleteTodo(e.target.closest('.todo-item'));
-    return;
-  }
-  if (e.target.classList.contains('checkbox')) {
-    markAsDone(e.target.closest('.todo-item').getElementsByClassName('text')[0]);
-  }
-});
-
-dtButton.addEventListener('click', switchThemes);
